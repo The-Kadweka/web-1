c7111c21e1636661d408c16c59bf8442052cf3c8
Hussein Kadweka <developersjuniors@gmail.com>
2019-10-22 22:47:53 +0300

    create templates file and display content on the template file

--- technlogy/urls.py
+++ technlogy/urls.py
@@ -18,5 +18,5 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^mashinani/',include('mashinani.urls'))
+    url(r'^',include('mashinani.urls'))
 ]
